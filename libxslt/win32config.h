f63988e17b04b43e456c922268266afecb889155
ccpaging <ccpaging@gmail.com>
2017-11-03 01:43:29 +0800

    Bug fixed

--- libxslt/win32config.h
+++ libxslt/win32config.h
@@ -79,11 +79,11 @@ static int isnan (double d) {
 #include <direct.h>
 
 /* snprintf emulation taken from http://stackoverflow.com/a/8712996/1956010 */
-#if defined(_MSC_VER) 
+#if defined(_MSC_VER) && _MSC_VER < 1900
 #if _MSC_VER < 1500
 #define snprintf _snprintf
 #define vsnprintf(b,c,f,a) _vsnprintf(b,c,f,a)
-#elseif _MSC_VER < 1900
+#else
 #include <stdarg.h>
 #include <stdio.h>
 
@@ -113,6 +113,7 @@ __inline int c99_snprintf(char *outBuf, size_t size, const char *format, ...)
 
     return count;
 }
+#endif /* _MSC_VER < 1500 */
 #endif /* defined(_MSC_VER) && _MSC_VER < 1900 */
 
 #define HAVE_SYS_STAT_H
